f110e284f3497e3339c6e2cf96e88fa52d4fa41b
abler <mail@xiechaoyi.com>
2017-07-13 03:02:49 +0800

    Signed-off-by: abler <mail@xiechaoyi.com>

--- client/start.bat
+++ client/start.bat
@@ -1,1 +1,26 @@
-ngrok -config ngrok.cfg start-all
+@echo off
+:: ===================
+:: 内网穿透客户端脚本
+:: ===================
+cls
+set port=80
+set host=www
+set /p port=请输入要映射的本地端口号（%port%）：
+:port
+if %port% gtr 99999999999999999999999999999999 (
+    cls
+    set /p port=请输入有效的端口号（0至65535）：
+    goto port
+) else if %port% leq 0 (
+    set port=Nan
+    goto port
+) else if %port% geq 65535 (
+    set port=Nan
+    goto port
+)
+:input
+set /p host=请输入要映射的子域名：
+if "%host%"=="www" (
+    goto input
+)
+ngrok -config ngrok.cfg -subdomain=%host% %port%
